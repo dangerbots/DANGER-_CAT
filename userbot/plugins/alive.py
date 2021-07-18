c45e3992ad0e2642413e6ce0363f01e1e3f05342
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-07-18 11:50:15 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,7 +8,7 @@ from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, rep
 DEFAULTUSER = ALIVE_NAME or "SurCat"
 CAT_IMG = Config.ALIVE_PIC
 CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ❣️ "
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
